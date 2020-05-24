b35aa75f716cde1d1cd6d8d038c5c0c1158f4239
cricri3007 <christophe.varengo@ens.uvsq.fr>
2020-05-24 19:06:55 +0200

    Create PersonnelType.java

---
+++ src/main/java/dao/PersonnelType.java
@@ -0,0 +1,11 @@
+package dao;
+
+import java.io.Serializable;
+
+public abstract class PersonnelType implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	abstract public boolean isGroupe();
+
+}
